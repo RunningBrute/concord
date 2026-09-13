6a9dc543474be50dc1a7a71799075bf206dad292
RunningBrute <piotrekbas8@gmail.com>
2026-09-13 21:24:31 +0200

    use rustfmt and format main file

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 use std::collections::HashMap;
 
-fn print_arguments(args: &Vec<String>)
-{
+fn print_arguments(args: &Vec<String>) {
     println!("Provided arguments:");
-    
-    for arg in args
-    {
+
+    for arg in args {
         println!("  •{}", arg);
     }
 }
 
-fn main()
-{
+fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     print_arguments(&args);
 
     let statistics: HashMap<String, i16> = HashMap::new();
-    for arg in args
-    {
-        match statistics.get(&arg)
-        {
+    for arg in args {
+        match statistics.get(&arg) {
             Some(word) => println!("New word found: {}", word),
-            None => println!("New word")
+            None => println!("New word"),
         }
     }
 }
