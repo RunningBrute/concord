505924a08d7786537a862afca190bc72675d57ed
RunningBrute <piotrekbas8@gmail.com>
2026-09-14 23:06:01 +0200

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::collections::HashMap;
 
+fn main() {
+    let words: Vec<String> = std::env::args().collect();
+
+    print_words(&words);
+
+    let _statistics: HashMap<String, i16> = get_words_stats(words);
+}
+
 fn print_words(words: &Vec<String>) {
     println!("Words from cli:");
 
@@ -8,22 +16,27 @@ fn print_words(words: &Vec<String>) {
     }
 }
 
-fn main() {
-    let words: Vec<String> = std::env::args().collect();
+fn get_words_stats(words: Vec<String>) -> HashMap<String, i16> {
+    let mut stats: HashMap<String, i16> = HashMap::new();
 
-    print_words(&words);
+    // words.swap_remove(0);
 
-    let mut statistics: HashMap<String, i16> = HashMap::new();
     for word in words {
-        match statistics.get_mut(&word) {
-            Some(value) => {
-                *value = *value + 1;
-                println!("Word already exist: {}, {}", word, *value);
-            }
-            None => {
-                println!("New word added: {}", word);
-                statistics.insert(word, 1);
-            }
+        match stats.get_mut(&word) {
+            Some(value) => update_word_frequency(&word, value),
+            None => add_new_word_to_stats(word, &mut stats),
         }
     }
+
+    return stats;
+}
+
+fn update_word_frequency(word: &String, frequency: &mut i16) {
+    *frequency = *frequency + 1;
+    println!("Word already exist: {}, {}", word, *frequency);
+}
+
+fn add_new_word_to_stats(word: String, stats: &mut HashMap<String, i16>) {
+    println!("New word added: {}", word);
+    stats.insert(word, 1);
 }
