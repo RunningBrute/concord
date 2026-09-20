2cdf0f24a24cad25a2c975dba2d3c93db5747d8f
RunningBrute <piotrekbas8@gmail.com>
2026-09-20 22:42:57 +0200

    cleanup main file - move stats code into proper mod

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod stats_printer;
 
 use file_reader::FileReader;
 use statistics::Statistics;
-use std::collections::BTreeMap;
+use statistics::get_all_stats;
 use std::path::Path;
 use std::str::SplitWhitespace;
 
@@ -14,7 +14,9 @@ fn main() {
     //let words: Vec<String> = std::env::args().collect();
     //print_words(&words);
     //let _statistics: BTreeMap<String, i16> = get_words_stats(words);
-    //let _result: Statistics = get_all_stats(words);
+    //let result: Statistics = get_all_stats(words);
+    //print_words_stats(&result.words());
+    //print_letters_stats(&result.letters());
 
     let file_path: &Path = Path::new("data/input.txt");
     let reader: FileReader = FileReader::new(&file_path);
@@ -35,51 +37,4 @@ fn main() {
 
     print_words_stats(&result.words());
     print_letters_stats(&result.letters());
-}
-
-fn get_all_stats(words: &[&str]) -> Statistics {
-    let mut result: Statistics = Statistics::new();
-
-    get_words_stats(words, &mut result.words_mut());
-    get_letters_stats(words, &mut result.letters_mut());
-
-    return result;
-}
-
-fn get_words_stats(words: &[&str], mut stats: &mut BTreeMap<String, i16>) {
-    // words.swap_remove(0);
-
-    for &word in words {
-        match stats.get_mut(word) {
-            Some(value) => update_word_frequency(&word, value),
-            None => add_new_word_to_stats(word, &mut stats),
-        }
-    }
-}
-
-fn get_letters_stats(words: &[&str], stats: &mut BTreeMap<char, i16>) {
-    for word in words {
-        for letter in word.chars() {
-            match stats.get_mut(&letter) {
-                Some(value) => {
-                    *value = *value + 1;
-                    println!("Letter already exist: {}, {}", letter, *value);
-                }
-                None => {
-                    println!("New letter added: {}", letter);
-                    stats.insert(letter, 1);
-                }
-            }
-        }
-    }
-}
-
-fn update_word_frequency(word: &str, frequency: &mut i16) {
-    *frequency = *frequency + 1;
-    println!("Word already exist: {}, {}", word, *frequency);
-}
-
-fn add_new_word_to_stats(word: &str, stats: &mut BTreeMap<String, i16>) {
-    println!("New word added: {}", word);
-    stats.insert(word.to_string(), 1);
 }
--- src/statistics.rs
+++ src/statistics.rs
@@ -31,3 +31,50 @@ impl Statistics {
         return &mut self.letters;
     }
 }
+
+pub fn get_all_stats(words: &[&str]) -> Statistics {
+    let mut result: Statistics = Statistics::new();
+
+    get_words_stats(words, &mut result.words_mut());
+    get_letters_stats(words, &mut result.letters_mut());
+
+    return result;
+}
+
+fn get_words_stats(words: &[&str], mut stats: &mut BTreeMap<String, i16>) {
+    // words.swap_remove(0);
+
+    for &word in words {
+        match stats.get_mut(word) {
+            Some(value) => update_word_frequency(&word, value),
+            None => add_new_word_to_stats(word, &mut stats),
+        }
+    }
+}
+
+fn get_letters_stats(words: &[&str], stats: &mut BTreeMap<char, i16>) {
+    for word in words {
+        for letter in word.chars() {
+            match stats.get_mut(&letter) {
+                Some(value) => {
+                    *value = *value + 1;
+                    println!("Letter already exist: {}, {}", letter, *value);
+                }
+                None => {
+                    println!("New letter added: {}", letter);
+                    stats.insert(letter, 1);
+                }
+            }
+        }
+    }
+}
+
+fn update_word_frequency(word: &str, frequency: &mut i16) {
+    *frequency = *frequency + 1;
+    println!("Word already exist: {}, {}", word, *frequency);
+}
+
+fn add_new_word_to_stats(word: &str, stats: &mut BTreeMap<String, i16>) {
+    println!("New word added: {}", word);
+    stats.insert(word.to_string(), 1);
+}
